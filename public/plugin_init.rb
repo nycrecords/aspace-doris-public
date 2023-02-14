2c18123654cc4725b328657a52ede6e3d2fd9e47
johnyu95 <jyu@records.nyc.gov>
2023-02-14 11:28:41 -0500

    Fixed bug with parse_repository_info function

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -635,4 +635,30 @@ Rails.application.config.after_initialize do
 
     end
   end
+
+  class Record
+    def parse_repository_info
+      info = {}
+      info['top'] = {}
+      unless resolved_repository.nil?
+        %w(name uri url parent_institution_name image_url repo_code).each do | item |
+          info['top'][item] = resolved_repository[item] unless resolved_repository[item].blank?
+        end
+        unless resolved_repository['agent_representation'].blank? || resolved_repository['agent_representation']['_resolved'].blank? || resolved_repository['agent_representation']['_resolved']['agent_contacts'].blank? || resolved_repository['agent_representation']['_resolved']['jsonmodel_type'] != 'agent_corporate_entity'
+          in_h = resolved_repository['agent_representation']['_resolved']['agent_contacts'][0]
+          %w{city region post_code country email }.each do |k|
+            info[k] = in_h[k] if in_h[k].present?
+          end
+          if in_h['address_1'].present?
+            info['address'] = []
+            [1,2,3].each do |i|
+              info['address'].push(in_h["address_#{i}"]) if in_h["address_#{i}"].present?
+            end
+          end
+          info['telephones'] = in_h['telephones'] if !in_h['telephones'].blank?
+        end
+      end
+      info
+    end
+  end
 end
